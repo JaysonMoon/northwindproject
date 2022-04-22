235719d03baa43b086852c896b295ae2a149057e
user <jayson.s.moon@gmail.com>
2022-04-22 11:21:28 -0400

    windows10

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class LoginForm extends JDialog{
     private JTextField tfUsername;
@@ -18,7 +19,7 @@ public class LoginForm extends JDialog{
         setModal(true);
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setVisible(true);
+
         btnOk.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -37,16 +38,56 @@ public class LoginForm extends JDialog{
                             JOptionPane.ERROR_MESSAGE);
                 }
             }
+        });setVisible(true);
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
         });
     }
 
     public User user;
     private User getAuthenticatedUser(String name, String password){
         User user = null;
+
+        final String DB_URL = "jdbc:mysql://localhost/northwind";
+        final String USERNAME = "root";
+        final String PASSWORD = "root";
+
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM users WHERE username=? AND password=?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                user = new User();
+                user.username = resultSet.getString("username");
+                user.password = resultSet.getString("password");
+            }
+
+            stmt.close();
+            conn.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         return user;
     }
 
     public static void main(String[] args) {
         LoginForm loginForm = new LoginForm(null);
+        User user = loginForm.user;
+        if (user != null){
+            System.out.println("Successful authentication of: " + user.username);
+
+        }
+        else {
+            System.out.println("Authentication Canceled");
+        }
     }
 }
