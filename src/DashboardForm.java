f3f322d8654ecbbec149e8812156adc36af895bb
user <jayson.s.moon@gmail.com>
2022-04-22 12:05:10 -0400

    commit

--- src/DashboardForm.java
+++ src/DashboardForm.java
@@ -2,95 +2,112 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.renderable.RenderableImage;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DashboardForm extends JFrame {
     private JPanel dashboardPanel;
     private JLabel lbAdmin;
-    private JButton brnRegister;
+    private JButton btnRegister;
 
+    public DashboardForm() {
+        setTitle("Dashborad");
+        setContentPane(dashboardPanel);
+        setMinimumSize(new Dimension(500, 429));
+        setSize(1200, 700);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-    public DashboardForm(){
-        setTitle("Dashboard");
-            setContentPane(dashboardPanel);
-            setMinimumSize(new Dimension(500, 429));
-            setSize(1200, 700);
-            setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        boolean hasRegistredUsers = connectToDatabase();
+        if (hasRegistredUsers) {
+            //show Login form
+            LoginForm loginForm = new LoginForm(this);
+            User user = loginForm.user;
 
-            boolean hasRegisteredUsers= connectToDatabase();
-            if(hasRegisteredUsers){
-                LoginForm loginForm = new LoginForm(this);
-                User user = loginForm.user;
-
-                if (user != null){
-                    lbAdmin.setText("User: " + user.username);
-                    setLocationRelativeTo(null);
-                    setVisible(true);
-                }
-                else{
-                    dispose();
-                }
+            if (user != null) {
+                lbAdmin.setText("User: " + user.username);
+                setLocationRelativeTo(null);
+                setVisible(true);
             }
-            else{
-                RegistrationForm registrationForm = new RegistrationForm(this);
-                User user = registrationForm.user;
-
-                if (user != null){
-                    lbAdmin.setText("User: " + user.username);
-                    setLocationRelativeTo(null);
-                    setVisible(true);
-                }
-                else { dispose();}
+            else {
+                dispose();
             }
-        brnRegister.addActionListener(new ActionListener() {
+        }
+        else {
+            //show Registration form
+            RegistrationForm registrationForm = new RegistrationForm(this);
+            User user = registrationForm.user;
+
+            if (user != null) {
+                lbAdmin.setText("User: " + user.username);
+                setLocationRelativeTo(null);
+                setVisible(true);
+            }
+            else {
+                dispose();
+            }
+        }
+        btnRegister.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 RegistrationForm registrationForm = new RegistrationForm(DashboardForm.this);
                 User user = registrationForm.user;
 
-                if (user != null){
+                if (user != null) {
                     JOptionPane.showMessageDialog(DashboardForm.this,
-                            "New User: " + user.username,
+                            "New user: " + user.username,
                             "Successful Registration",
                             JOptionPane.INFORMATION_MESSAGE);
-
                 }
             }
         });
     }
 
-    private boolean connectToDatabase(){
-        boolean hasRegisteredUsers = false;
+    private boolean connectToDatabase() {
+        boolean hasRegistredUsers = false;
 
+        final String MYSQL_SERVER_URL = "jdbc:mysql://localhost/";
         final String DB_URL = "jdbc:mysql://localhost/northwind";
         final String USERNAME = "root";
         final String PASSWORD = "root";
 
         try{
-            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            //First, connect to MYSQL server and create the database if not created
+            Connection conn = DriverManager.getConnection(MYSQL_SERVER_URL, USERNAME, PASSWORD);
             Statement statement = conn.createStatement();
-            statement.executeUpdate("CREATE DATABASE IF NOT EXISTS northwind");
+            statement.executeUpdate("CREATE DATABASE IF NOT EXISTS MyStore");
             statement.close();
             conn.close();
+
+            //Second, connect to the database and create the table "users" if cot created
+            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            statement = conn.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS users ("
+                    + "username VARCHAR(200) NOT NULL,"
+                    + "password VARCHAR(200) NOT NULL"
+                    + ")";
+            statement.executeUpdate(sql);
+
+            //check if we have users in the table users
             statement = conn.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM users");
 
-            if(resultSet.next()){
+            if (resultSet.next()) {
                 int numUsers = resultSet.getInt(1);
-                if(numUsers > 0){
-                    hasRegisteredUsers = true;
+                if (numUsers > 0) {
+                    hasRegistredUsers = true;
                 }
             }
 
             statement.close();
             conn.close();
-        } catch (Exception e) {
+
+        }catch(Exception e){
             e.printStackTrace();
         }
 
-        return hasRegisteredUsers;
-
+        return hasRegistredUsers;
     }
 
     public static void main(String[] args) {
