2b3f2758a6e34e8288a41bf7a67d6e8cff5514ae
User <jayson.s.moon@gmail.com>
2022-04-21 18:37:36 -0700

    init

---
+++ src/LoginForm.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class LoginForm {
+    private JTextField tfUsername;
+    private JPasswordField pfPassword;
+    private JButton btnOk;
+    private JButton btnCancel;
+    private JPanel loginPanel;
+}
