5e63743692aa5bf64f2388b494dad577db0a4f3d
user <jayson.s.moon@gmail.com>
2022-04-25 22:37:31 -0400

    commit

--- src/EditDatabase.java
+++ src/EditDatabase.java
@@ -1,19 +1,19 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.awt.*;
 
 public class EditDatabase extends JFrame{
     private JPanel panelEdit;
-       private JButton btnCustomers;
-    private JScrollPane table;
+       private JButton btnShippers;
+    private JTable tableShippers;
 
     public EditDatabase() {
 
+        setTitle("Login");
+        setContentPane(panelEdit);
+        setMinimumSize(new Dimension(450, 474));
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
+
 
     public static void main(String[] args) {
         EditDatabase database = new EditDatabase();
